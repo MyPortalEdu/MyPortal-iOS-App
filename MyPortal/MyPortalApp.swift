45401445fd12453faeca8dc5b4fc391a5947a784
Rowan Richards <rwnrchrds@gmail.com>
2026-05-16 21:14:06 +0100

    Initial Commit

---
+++ MyPortal/MyPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortalApp.swift
+//  MyPortal
+//
+//  Created by Rowan Richards on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPortalTests/MyPortalTests.swift
@@ -0,0 +1,19 @@
+//
+//  MyPortalTests.swift
+//  MyPortalTests
+//
+//  Created by Rowan Richards on 16/05/2026.
+//
+
+import Testing
+@testable import MyPortal
+
+struct MyPortalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
